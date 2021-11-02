ef87a0cce2df789becccb50238c988e0cb06c1e2
Bruzsa László <lacika007@gmail.com>
2021-11-02 15:52:13 +0100

    day01

---
+++ src/main/java/day01/Movie.java
@@ -0,0 +1,43 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private int year;
+    List<Actor> actors;
+
+    public Movie(String title, int year) {
+        this.title = title;
+        this.year = year;
+        this.actors = new ArrayList<>();
+    }
+
+    public boolean addActors(Actor actor) {
+        return actors.add(actor);
+    }
+
+    public int actorsInTheirTwenties() {
+        int sum = 0;
+        for (Actor a : actors)
+            if (year - a.getYearOfBirth() < 30 &&
+                    year - a.getYearOfBirth() >= 20)
+                sum++;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Movie movie = new Movie("Terminator", 1984);
+        movie.addActors(new Actor("Arnold", 1947));
+        movie.addActors(new Actor("Linda", 1956));
+        movie.addActors(new Actor("Shawn", 1961));
+        movie.addActors(new Actor("Pistike", 1980));
+
+        System.out.println(movie.actorsInTheirTwenties());
+
+    }
+
+}
